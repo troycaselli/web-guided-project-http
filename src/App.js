fa82db3a59ae958180809f9a3a64e345d52b4b62
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-31 12:30:08 -0700

    todo list created with mock server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {getTodos, postTodo, putTodo, deleteTodo} from './actions/todos';
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
+    getTodos()
+      .then(res => {
+        setTodos(res);
+      })
+  }
+
+  const addTodo = () => {
+    postTodo(todo)
+      .then(() => {
+        getData();
+      })
+  }
+
+  const completeTodo = (todo) => {
+    const newTodo = {...todo, isDone: true};
+    putTodo(newTodo)
+      .then(() => getData());
+  }
+
+  const deleteTodoItem = (id) => {
+    deleteTodo(id)
+      .then(() => getData());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={addTodo} >submit</button>
+      {todos.map((todo, idx) => (
+        <div key={idx}>
+          <span className={todo.isDone ? 'done' : ''}>{todo.description}</span>
+          <span>
+            {todo.isDone ? <button onClick={() => deleteTodoItem(todo.id)}>Delete</button> : <button onClick={() => completeTodo(todo)}>Complete</button>}
+          </span>
+          </div>
+      ))}
     </div>
   );
 }
